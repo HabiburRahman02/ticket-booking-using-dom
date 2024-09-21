d23400635a74aecee58a0452b1cb6994dff8c5b5
Habibur Rahman <habibur88454@gmail.com>
2024-09-21 20:16:31 +0600

    total price calculate and seat booking limitation

--- js/booking.js
+++ js/booking.js
@@ -1,6 +1,22 @@
 
 let selected = [];
+let totalPrice = 0
 function handleSelectedSeat(seat) {
+    
+    if (selected.includes(seat.innerText)) {
+        alert('Seat already added');
+        return;
+    }
+
+    console.log(selected);
+    if (selected.length >= 4) {
+        alert('One person booked only 4 seat');
+        return;
+    }
+
+    totalPrice += 550;
+    document.getElementById('total-price').innerText = totalPrice.toFixed('2')
+
     seat.classList.add('bg-black', 'text-white')
     // booked seat increase
     const bookedSeat = document.getElementById('booked-seat')
@@ -12,11 +28,10 @@ function handleSelectedSeat(seat) {
     const totalSeat = parseInt(totalSeatStr.innerText);
     totalSeatStr.innerText = totalSeat - 1;
 
-
     const selectedSeat = document.getElementById('selected-seat');
-    // selectedSeat.innerHTML = `<li class="flex justify-between">
-    //                     <span>${seat.innerText}</span>
-    //                     <span>Economy</span>
-    //                     <span>550</span>
-    //                 </li>`
+    selectedSeat.innerHTML += `<li class="flex justify-between">
+                        <span>${seat.innerText}</span>
+                        <span>Economy</span>
+                        <span>550</span>
+                    </li>`
 }
